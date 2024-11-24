6170216744e35ce31087c11b33b3d7811754cedb
ShadowFlyX <lascot.lexa@mail.ru>
2024-11-24 17:49:47 +0300

    tested mail notification service. Added requirements.txt with all python dependencies

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from datetime import datetime
+from .utils import send_email
+
+def send_welcome_email(request):
+    subject = "Добро пожаловать!"
+    to_email = "vlisichkin2004@gmail.com"
+    context = {
+        "subject": subject,
+        "message": "Спасибо за регистрацию на нашем сайте. Мы рады вас приветствовать!",
+        "year": datetime.now().year
+    }
+    send_email(subject, to_email, "email/index.html", context)
+    return HttpResponse("Письмо отправлено")
--- backend/innosite/settings.py
+++ backend/innosite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER") 
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
