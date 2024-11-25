a708681f150088e080ede3a45ad957ef550fa10d
ShadowFlyX <lascot.lexa@mail.ru>
2024-11-25 22:36:42 +0300

    again renamed

--- backend/innosite/settings.py
+++ backend/innosite/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -144,3 +144,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
