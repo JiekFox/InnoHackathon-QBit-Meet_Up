fbabfa1b52c64770366181ed274780cd8241a37f
ShadowFlyX <lascot.lexa@mail.ru>
2024-11-28 19:47:46 +0300

    hotfix

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -16,10 +16,3 @@ class IsStaff(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.is_staff
 
-class IsOwner(BasePermission):
-    """
-    Разрешает доступ для владельца аккаунта
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.user
