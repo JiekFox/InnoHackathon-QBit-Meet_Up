b855390fb4259cd65fde9501fd7e8abf1595498e
ShadowFlyX <lascot.lexa@mail.ru>
2024-11-28 19:18:53 +0300

    trying to fix cache and also created new enpoint to check wether user is subscribed to meeting

--- backend/api/cache_control.py
+++ backend/api/cache_control.py
@@ -1,7 +1,15 @@
 from django.core.cache import cache
 
-def clear_meetings_cache():
-    cache.clear()
+# Попытки сделать очистку кеша независимо друг от друга... Пока чет не очень
 
 def clear_users_cache():
-    ...
+    """
+    Очистка кэша для всех путей, связанных с пользователями.
+    """
+    cache.delete('users_list')
+
+def clear_meetings_cache():
+    """
+    Очистка кэша для всех путей, связанных со встречами.
+    """
+    cache.delete('meetings_list')
