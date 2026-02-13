6b138dca83a08c236c02cd5daf3dfd0ba6eccca2
ShadowFlyX <lascot.lexa@mail.ru>
2026-02-13 11:39:56 +0300

    feat: updated requirements.txt and standardized errors

--- backend/innosite/settings.py
+++ backend/innosite/settings.py
@@ -56,6 +56,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "EXCEPTION_HANDLER": "drf_standardized_errors.handler.exception_handler",
 }
 
 
