a4c53129c1498503c5ca36a88c98b49d50018284
ShadowFlyX <lascot.lexa@mail.ru>
2026-03-14 19:50:41 +0300

    feat: added is_active field to filter out non-actual meetups

--- backend/api/views/filters/filters.py
+++ backend/api/views/filters/filters.py
@@ -1,6 +1,7 @@
 import django_filters
 from api.models import Tag
 from api.models.meeting import Meeting
+from django.utils import timezone
 from django_filters import BooleanFilter, CharFilter, FilterSet, IsoDateTimeFilter
 
 
@@ -12,7 +13,13 @@ class MeetingFilter(FilterSet):
     tags = django_filters.ModelMultipleChoiceFilter(
         field_name="tags__id", to_field_name="id", queryset=Tag.objects.all(), conjoined=False
     )
+    is_active = BooleanFilter(method="filter_is_active", label="Только актуальные")
 
     class Meta:
         model = Meeting
-        fields = ["location", "is_online", "tags"]
+        fields = ["location", "is_online", "tags", "is_active"]
+
+    def filter_is_active(self, queryset, name, value):
+        if value:
+            return queryset.filter(datetime_beg__gt=timezone.now())
+        return queryset
